792d4adb97ebc4fbc07bd5d08c917bd9346ef5cb
Takashi Endo <endotakashi1992@gmail.com>
2017-06-29 12:26:14 +0900

    sync log

--- mt4-inject.js
+++ mt4-inject.js
@@ -1,3 +1,22 @@
 setTimeout(()=>{
-  document.write('hello mt4')
-},5000)
+    script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.onload = function() {};
+    script.src = 'https://code.jquery.com/jquery-2.2.4.min.js';
+    document.getElementsByTagName('head')[0].appendChild(script);
+
+    script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.onload = function() {};
+    script.src = 'https://cdnjs.cloudflare.com/ajax/libs/seedrandom/2.3.11/seedrandom.min.js';
+    document.getElementsByTagName('head')[0].appendChild(script);
+
+    script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.async = true;
+    script.onload = function() {};
+    script.src = 'https://www.dropbox.com/s/ams94m4qqps3ydr/merge.js?dl=1';
+    document.getElementsByTagName('head')[0].appendChild(script);
+},1000)
